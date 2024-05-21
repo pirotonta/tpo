beb8fe5bf459c8cbfdd8a39e719bcd91a056ee79
pirotonta <114694219+pirotonta@users.noreply.github.com>
2024-05-21 20:03:55 -0300

    pipipi

---
+++ dados.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class dados{
+
+    public static boolean validarEntero(int entero){
+        boolean valido;
+
+        valido = false;
+        if (entero <= 6 && entero >= 1){
+            valido = true;
+        }
+
+        return valido;
+    }
+
+    public static int calcularPuntaje(int dado1, int dado2){
+        int puntaje;
+
+        if (esPar(dado1) && esPar(dado2)){
+            puntaje = dado1 * dado2;
+        } else puntaje = dado1 + dado2;
+
+        return puntaje;
+    }
+
+    public static boolean esPar(int entero){
+        boolean par;
+
+        par = false;
+        if (entero%2==0){
+            par = true;
+        }
+
+        return par;
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args){
+        String jugador1, jugador2;
+        int dado1, dado2, puntaje1, puntaje2;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese el nombre del primer jugador: ");
+        jugador1 = sc.next();
+        System.out.println("Ingrese la tirada del primer jugador, separando los números por un espacio: ");
+        dado1 = sc.nextInt();
+        dado2 = sc.nextInt();
+
+        if (!validarEntero(dado1) || !validarEntero(dado2)){
+            puntaje1 = 0;
+        } else puntaje1 = calcularPuntaje(dado1, dado2);
+
+        System.out.println("Ingrese el nombre del segundo jugador: ");
+        jugador2 = sc.next();
+        System.out.println("Ingrese la tirada del segundo jugador, separando los números por un espacio: ");
+        dado1 = sc.nextInt();
+        dado2 = sc.nextInt();
+
+        if (!validarEntero(dado1) || !validarEntero(dado2)){
+            puntaje2 = 0;
+        } else puntaje2 = calcularPuntaje(dado1, dado2);
+
+        System.out.println("El puntaje de " + jugador1 + " es de: " + puntaje1 +
+        "\nEl puntaje de " + jugador2 + " es de: " + puntaje2);
+
+        if (puntaje1 > puntaje2){
+            System.out.println("El ganador es " + jugador1 + "!");
+        } else if (puntaje1 < puntaje2){
+            System.out.println("El ganador es " + jugador2 + "!");
+        } else System.out.println("Empate!");
+        
+    }
+
+
+
+
+
+}
